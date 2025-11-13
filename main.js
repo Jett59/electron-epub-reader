f45b85d00c245b6b20623254209b74414ffecd0d
Jett59 <jett.thompson.59@gmail.com>
2025-11-13 14:05:22 +1100

    add an electron project with react (vite) client

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    if (process.env.ELECTRON_EPUB_READER_MODE === 'dev') {
+        win.loadURL('http://localhost:5173')
+    } else {
+        win.loadFile('index.html')
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
